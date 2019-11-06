6abb9cf50a3d8a9c1e01ed853b39b50536407430
itamiranks <dungpc@sphinx-software.com>
2019-11-06 13:54:00 +0700

    delete log

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ const app = new Koa();
 const router = new Router();
 
 router.get('/search/:name', async (ctx) => {
-    console.log("aaa");
+
     let users = await knex.select('*').from('guitarist').where('name', 'like', '%' + ctx.params.name + '%' );
-    console.log(users);
+
     if (users) {
         ctx.body = nunjucks.render('user.html', { users });
     } else {
         ctx.body = { message : 'Not found' }
     }
+
 });
 
 app.use(router.routes());
